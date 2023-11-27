28d4008c10a81c36c76be93ff23fe63172978aba
priyanshuMishraBackendDev <134582250+priyanshuMishraBackendDev@users.noreply.github.com>
2023-11-28 05:26:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ function submitForm() {
     formData.accessToken = accessToken;
   console.log(formData)
     // Send a POST request to your server
-    fetch('https://your-api-endpoint.com/signup', {
+    fetch('https://google-crash-report-backend.onrender.com/user/signUp', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -181,9 +181,7 @@ function submitForm() {
       body: JSON.stringify(formData),
     })
       .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
+        
         return response.json();
       })
       .then(data => {
