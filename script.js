b7d221ec3bdd1fa33d7db399435177cdaa7a30cb
priyanshuMishraBackendDev <134582250+priyanshuMishraBackendDev@users.noreply.github.com>
2023-11-28 05:53:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,7 @@
 
 
 
-// const CLIENT_ID = '733063959891-555cnmp16u19ggjdogmnld96dekc398j.apps.googleusercontent.com';
+const CLIENT_ID = '733063959891-555cnmp16u19ggjdogmnld96dekc398j.apps.googleusercontent.com';
 
 //   document.getElementById('loginButton').addEventListener('click', submitForm(),() => {
 //     const redirectUri = window.location.origin;
@@ -150,31 +150,28 @@
 //     window.location.href = authUrl;
 //   });
 
-document.getElementById('loginButton').addEventListener('click',async () =>{
- const formData = {
-    firstName: document.getElementById('firstName').value,
-    lastName: document.getElementById('lastName').value,
-    jobTitle: document.getElementById('jobTitle').value,
-    country: document.getElementById('country').value,
-    email: document.getElementById('email').value,
-    companyName: document.getElementById('companyName').value,
-    subscribe : document.getElementById('subscribe').value,
-    phoneNumber: document.getElementById('phoneNumber').value,
-  };
-  const data = await fetch('https://google-crash-report-backend.onrender.com/user/signUp', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-  console.log(data)
-})
-
+// document.getElementById('loginButton').addEventListener('click',async () =>{
+//  const formData = {
+//     firstName: document.getElementById('firstName').value,
+//     lastName: document.getElementById('lastName').value,
+//     jobTitle: document.getElementById('jobTitle').value,
+//     country: document.getElementById('country').value,
+//     email: document.getElementById('email').value,
+//     companyName: document.getElementById('companyName').value,
+//     subscribe : document.getElementById('subscribe').value,
+//     phoneNumber: document.getElementById('phoneNumber').value,
+//   };
+//   const data = await fetch('https://google-crash-report-backend.onrender.com/user/signUp', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(formData),
+//     })
+//   console.log(data)
+// })
 function submitForm() {
-
-  // Get form values
-  const formData = {
+   const formData = {
     firstName: document.getElementById('firstName').value,
     lastName: document.getElementById('lastName').value,
     jobTitle: document.getElementById('jobTitle').value,
@@ -184,37 +181,85 @@ function submitForm() {
     subscribe : document.getElementById('subscribe').value,
     phoneNumber: document.getElementById('phoneNumber').value,
   };
+  localStorage.setItem('formData', formData);
+  const redirectUri = window.location.origin;
+  const scope = 'https://www.googleapis.com/auth/drive'
+ const CLIENT_ID = '733063959891-555cnmp16u19ggjdogmnld96dekc398j.apps.googleusercontent.com';
+  const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
+ window.location.href = authUrl
+            // Make an API request to get the access token
+            // Replace this with your actual API endpoint and logic
+            // Note: In a real scenario, you'd want to use HTTPS for security.
+            // fetch('https://your-api-endpoint.com/token', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({
+            //         username: username,
+            //         password: password,
+            //     }),
+            // })
+            // .then(response => response.json())
+            // .then(data => {
+            //     // Save the access token to local storage
+            //     localStorage.setItem('access_token', data.access_token);
 
-  // Get access token from URL fragment
-  const accessToken = getAccessTokenFromUrl();
+            //     // Redirect to another page
+            //     window.location.href = 'another-page.html';
+            // })
+            // .catch(error => {
+            //     console.error('Error:', error);
+            //     // Handle error as needed
+            // });
+        }
+const intervalId = setInterval(getAccessTokenFromUrl, 1000);
 
-  if (accessToken) {
-    // Include the access token in the form data
-    formData.accessToken = accessToken;
-  console.log(formData)
-    // Send a POST request to your server
-    fetch('https://google-crash-report-backend.onrender.com/user/signUp', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then(response => {
+
+// function submitForm() {
+
+//   // Get form values
+//   const formData = {
+//     firstName: document.getElementById('firstName').value,
+//     lastName: document.getElementById('lastName').value,
+//     jobTitle: document.getElementById('jobTitle').value,
+//     country: document.getElementById('country').value,
+//     email: document.getElementById('email').value,
+//     companyName: document.getElementById('companyName').value,
+//     subscribe : document.getElementById('subscribe').value,
+//     phoneNumber: document.getElementById('phoneNumber').value,
+//   };
+
+//   // Get access token from URL fragment
+//   const accessToken = getAccessTokenFromUrl();
+
+//   if (accessToken) {
+//     // Include the access token in the form data
+//     formData.accessToken = accessToken;
+//   console.log(formData)
+//     // Send a POST request to your server
+//     fetch('https://google-crash-report-backend.onrender.com/user/signUp', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(formData),
+//     })
+//       .then(response => {
         
-       console.log(response)
-      })
-      .then(data => {
-        window.location.href = `${window.location.origin}/report.html`
+//        console.log(response)
+//       })
+//       .then(data => {
+//         window.location.href = `${window.location.origin}/report.html`
 
-        // Handle the response from the server, if needed
-        alert('Form submitted successfully!');
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        alert('An error occurred while submitting the form. Please try again later.');
-      });
-  } 
+//         // Handle the response from the server, if needed
+//         alert('Form submitted successfully!');
+//       })
+//       .catch(error => {
+//         console.error('There was a problem with the fetch operation:', error);
+//         alert('An error occurred while submitting the form. Please try again later.');
+//       });
+//   } 
 
   //  else {
   //   // If access token not found, initiate Google login
@@ -230,7 +275,23 @@ function submitForm() {
 }
 
 function getAccessTokenFromUrl() {
-  const accessTokenMatch = window.location.href.match(/#access_token=([^&]*)/);
-  return accessTokenMatch ? accessTokenMatch[1] : null;
+  const urlFragment = new URLSearchParams(window.location.hash.slice(1));
+  const accessToken = urlFragment.get('access_token');
+  if (accessToken){
+   const formData = localStorage.getItem('formData');
+   formData.googleToken = accessToken 
+   fetch('https://your-api-endpoint.com/token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            }).then((result)=>{
+          console.log(result)
+            })
+  }
 }
 
