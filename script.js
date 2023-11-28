41724514e85d3f6286310499f69e22d7fdaa9079
priyanshuMishraBackendDev <134582250+priyanshuMishraBackendDev@users.noreply.github.com>
2023-11-28 05:33:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -139,17 +139,37 @@
 
 
 
-const CLIENT_ID = '733063959891-555cnmp16u19ggjdogmnld96dekc398j.apps.googleusercontent.com';
+// const CLIENT_ID = '733063959891-555cnmp16u19ggjdogmnld96dekc398j.apps.googleusercontent.com';
 
-  document.getElementById('loginButton').addEventListener('click', submitForm(),() => {
-    const redirectUri = window.location.origin;
-    const scope = 'https://www.googleapis.com/auth/drive';
+//   document.getElementById('loginButton').addEventListener('click', submitForm(),() => {
+//     const redirectUri = window.location.origin;
+//     const scope = 'https://www.googleapis.com/auth/drive';
 
-    const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
-    console.log(authUrl)
-    window.location.href = authUrl;
-  });
+//     const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
+//     console.log(authUrl)
+//     window.location.href = authUrl;
+//   });
 
+document.getElementById('loginButton').addEventListener('click',async () =>{
+ const formData = {
+    firstName: document.getElementById('firstName').value,
+    lastName: document.getElementById('lastName').value,
+    jobTitle: document.getElementById('jobTitle').value,
+    country: document.getElementById('country').value,
+    email: document.getElementById('email').value,
+    companyName: document.getElementById('companyName').value,
+    subscribe : document.getElementById('subscribe').value,
+    phoneNumber: document.getElementById('phoneNumber').value,
+  };
+  const data = await fetch('https://google-crash-report-backend.onrender.com/user/signUp', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+  console.log(data)
+})
 
 function submitForm() {
 
